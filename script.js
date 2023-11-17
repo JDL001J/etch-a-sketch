9f55aa8c5411a67a0229acd7aeadb7958e376fda
JDL001J <116929245+JDL001J@users.noreply.github.com>
2023-11-17 15:53:57 -0600

    created grid still need to fix rows and columns

--- script.js
+++ script.js
@@ -3,19 +3,29 @@ size = document.querySelector(".size");
 btn = document.querySelector('.btn');
 
 function getSize(){
-    gridSize = size.target.value;
-    createGrid(gridSize)
+    
+    gridSize = size.value;
+    return gridSize
 }
 
 
-function createGrid(size){
-gridSize = getSize();
-  for(i = 0; i<size; i++){
+function createGrid(){
+    deleteGrid();
+    gridSize = getSize();
+  for(i = 0; i<gridSize; i++){
     div = document.createElement('div');
     div.classList.add("item");
     grid.appendChild(div);
     }
   }  
 
+  function deleteGrid(){
+    items = document.querySelectorAll(".item");
+    elm = document.querySelector(".item");
+    length = items.length;
+    for(i = 0; i<length;i++){
+        grid.removeChild(items[i])
+    }
+  }
 
-  btn.addEventListener('click', getSize);
+  btn.addEventListener('click', createGrid);
