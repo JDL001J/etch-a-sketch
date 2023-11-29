c0c2f6065c7f1b3b3a3a7c0752bda3ea032a92f6
JDL001J <116929245+JDL001J@users.noreply.github.com>
2023-11-28 20:15:29 -0600

    added color changeablility, mouse events

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 grid = document.querySelector('.grid')
 size = document.querySelector(".size");
 btn = document.querySelector('.btn');
+color = document.querySelector(".color-wheel");
+color.addEventListener("mouseleave", ()=>{
+  drawFunc()
+ 
+}
+
+)
+
+
+function createItems(){
+  items = document.querySelectorAll('.item');
+  return items
+}
 
 function getSize(){
     
-    gridSize = size.value;
+    // gridSize = size.value;
+    gridSize = 900;
+   
+
+   
     return gridSize
 }
 
@@ -15,17 +32,45 @@ function createGrid(){
   for(i = 0; i<gridSize; i++){
     div = document.createElement('div');
     div.classList.add("item");
+   
     grid.appendChild(div);
     }
+    drawFunc();
   }  
 
   function deleteGrid(){
-    items = document.querySelectorAll(".item");
-    elm = document.querySelector(".item");
+    
+   createItems();
     length = items.length;
     for(i = 0; i<length;i++){
         grid.removeChild(items[i])
     }
   }
+function drawFunc(){
+createItems();
+getColor();
+    items.forEach(elm => {
+      
+        
+        elm.addEventListener('mouseover', ()=>{
+          getColor();
+          elm.style.backgroundColor = `${color}`;
+        })
+        
+      
+    });
+  };
+
+function getColor(){
+  color = document.querySelector(".color-wheel");
+color = color.value;
+return color
+
+}
+ 
+ 
+
 
   btn.addEventListener('click', createGrid);
+
+  createGrid()
